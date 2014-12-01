5c1e19bddea1d285816e8a494663cc6bb70f23a2
Michele Carignani <michele.carignani@gmail.com>
2014-12-01 20:03:37 +0100

    sfide visibili in home page

--- functions.php
+++ functions.php
@@ -92,4 +92,12 @@ function no_nopaging($query) {
 
 add_action('parse_query', 'no_nopaging');
 
+function my_get_posts( $query ) {
+	if ( is_home() && empty($query->query_vars['suppress_filters'] ))
+		$query->set( 'post_type', array('post', 'sfida_event') );
+    return $query;
+ }  
+
+add_filter( 'pre_get_posts', 'my_get_posts' );
+
 ?>
